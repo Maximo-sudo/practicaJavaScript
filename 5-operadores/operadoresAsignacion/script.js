119e112d8f9c6a44ab0c49e28d4d860886e3967d
Maximo-sudo <maximovm2005@gmail.com>
2025-03-30 15:34:36 +0200

    Operadores de asignación

---
+++ 5-operadores/operadoresAsignacion/script.js
@@ -0,0 +1,35 @@
+//operadoresAsignacion-Incremento-Decremento
+
+let a = 1;
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a++; //incrementa a en 1
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a+=1; //incrementa a en 1, sumando lo que yo defina
+document.write('valor de a: ', a);
+document.write('<br>');
+a+=10; 
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a--; //decrementa a en 1
+document.write('valor de a: ', a);
+document.write('<br>');
+a-=10; // lo mismo pero restando en 10
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a*=2; //multiplica a en 2
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a/=2; //divide a en 2
+document.write('valor de a: ', a);
+document.write('<br>');
+
+a**=2; //eleva a la potencia de 2
+document.write('valor de a: ', a);
+
