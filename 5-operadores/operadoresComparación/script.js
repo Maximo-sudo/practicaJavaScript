f9f1baacb1f8a9f75cd49fc9f0cae80bbaa730c7
Maximo-sudo <maximovm2005@gmail.com>
2025-03-30 15:40:14 +0200

    Operadores de comparación

---
+++ 5-operadores/operadoresComparación/script.js
@@ -0,0 +1,23 @@
+let valor1, valor2;
+
+valor1 = 10;
+valor2 = 10;
+
+document.write(valor1 > valor2); //compara si valor1 es mayor que valor2
+document.write('<br>');
+
+document.write(valor1 < valor2); //comara si valor1 es menor que valor2
+document.write('<br>');
+
+document.write(valor1 == valor2); //compara si valor1 es igual a valor2
+document.write('<br>');
+
+document.write(valor1 != valor2); //compara si valor1 es diferente a valor2
+document.write('<br>');
+
+document.write(valor1 >= valor2); //compara si valor1 es mayor o igual a valor2
+document.write('<br>');
+
+document.write(valor1 <= valor2); //compara si valor1 es menor o igual a valor2
+document.write('<br>');
+
