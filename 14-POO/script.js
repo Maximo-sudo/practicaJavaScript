1926a3041affa05e5a20e65e0a3bb6fb2df205fd
Maximo-sudo <maximovm2005@gmail.com>
2025-03-31 21:40:09 +0200

    POO

---
+++ 14-POO/script.js
@@ -0,0 +1,20 @@
+var Auto = {
+    marca : 'Toyota',
+    model: 'Celica',
+    anio: 1994,
+    color: 'Negro',
+    tipo: 'Supra',
+    radio: ['Los 40', 'Kiss FM', 'Cadena 100'],
+}
+
+document.write('Marca: ', Auto.marca);
+document.write('<br>');
+document.write('Modelo: ', Auto.model);
+document.write('<br>');
+document.write('Año: ', Auto.anio);
+document.write('<br>');
+document.write('Color: ', Auto.color);
+document.write('<br>');
+document.write('Tipo: ', Auto.tipo);
+document.write('<br>');
+document.write('Radio: ', Auto.radio);
