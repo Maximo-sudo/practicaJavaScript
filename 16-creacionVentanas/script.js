b5f32772d9d5f1e22aee7b1155d078200dfa2e0c
Maximo-sudo <maximovm2005@gmail.com>
2025-04-03 19:36:03 +0200

    Creación De ventanas

---
+++ 16-creacionVentanas/script.js
@@ -0,0 +1,16 @@
+function abrir() {
+    let ventana = open();
+    ventana.document.write('Segunda Pagina Web');
+}
+
+function ventana() {
+    let ventana = open('','','width=400, height=250');
+    ventana.document.write('Soy Otra Ventana')
+}
+
+function confirmar() {
+    let respuesta = confirm('Quieres Salir De La Pagina');
+    if (respuesta == true) {
+        window.close();
+    }
+}
