fd43aab93d76f95c7de612593c5bfe9410c1a3d8
Maximo-sudo <maximovm2005@gmail.com>
2025-04-04 20:24:13 +0200

    Propiedades De La Pantalla O Navegador

---
+++ 18-propPantalla-Navegador/script.js
@@ -0,0 +1,16 @@
+// muestraLaAlturaDeLaPantalla
+document.write('availHeight: ', screen.availHeight, '<br>');
+// muestraElAnchoDeLaPantalla
+document.write('availWidth: ', screen.availWidth, '<br>');
+// muestraLaProfundidadDeLaPantalla
+document.write('colorDepth: ', screen.colorDepth, '<br>', '<br>');
+// propiedadesDeLaPantalla
+document.write('Propiedades de la pantalla:', '<br>');
+// muestraElNombreDelNavegador
+document.write('appName: ', navigator.appName, '<br>');
+// muestraLaVersionDelNavegador
+document.write('appVersion: ', navigator.appVersion, '<br>');
+// muestraSiLasCookiesEstanActivadas
+document.write('cookieEnabled: ', navigator.cookieEnabled, '<br>');
+// muestraPluginsDelNavegador
+document.write('plugins: ', navigator.plugins.length, '<br>');
