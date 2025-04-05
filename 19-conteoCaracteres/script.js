cd9e18bbf42d2f06b47f6bb44ea19ce62779585a
Maximo-sudo <maximovm2005@gmail.com>
2025-04-05 20:52:28 +0200

    conteo caracteres 2.0

--- 19-conteoCaracteres/script.js
+++ 19-conteoCaracteres/script.js
@@ -1,15 +1,26 @@
 document.getElementById('texto').addEventListener('keyup', caracteres);
 
+
 function caracteres() {
     const MAX_CARACTERES = 20;
     let texto = document.getElementById('texto').value;
+    document.getElementById('texto').style.background = '';
     let cant = texto.length;
     let disponible = MAX_CARACTERES - cant;
 
     document.getElementById('cantidad').textContent = disponible;
     if (disponible <= 0){
         swal.fire('Caracteres agotados');
+        document.getElementById('texto').style.background = 'red';
         document.getElementById('texto').value = "";
         document.getElementById('caracteres').textContent = 20;
+        
     }
 }
+
+
+// function blur() {
+//     if (document.getElementById('texto').value == '') {
+        
+//     }
+// }
