5574f9fbcc28eb6abfbb8903a6e869f7a60683b6
Maximo-sudo <maximovm2005@gmail.com>
2025-04-07 14:49:57 +0200

    Alarma con sonido

---
+++ 20-temporizador/2-conAudio/script.js
@@ -0,0 +1,32 @@
+let elementoAlarma = document.getElementById('textoAlarma');
+let elementoSonidoAlarma = document.getElementById('audioAlarma');
+
+function startTimer() {
+    const segundos = document.getElementById('timeSelect').value;
+    let timeOut = parseInt(segundos);
+    
+    if (isNaN(timeOut) || timeOut <= 0) {
+        Swal.fire('Ingresa un tiempo válido');
+        return;
+    }
+
+    const timerElement = document.getElementById('timer');
+    const interval = setInterval(() => {
+        const minutes = Math.floor(timeOut / 60);
+        const seconds = timeOut % 60;
+        timerElement.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        
+        if (timeOut <= 0) {
+            clearInterval(interval);
+            tiempoCumplido();
+            Swal.fire('¡Se terminó el tiempo!');
+        }
+        timeOut--;
+    }, 1000);
+}
+
+function tiempoCumplido() {
+    elementoAlarma.textContent = "ENCENDIDO";
+    elementoAlarma.style.color = "green";
+    elementoSonidoAlarma.play();
+}
