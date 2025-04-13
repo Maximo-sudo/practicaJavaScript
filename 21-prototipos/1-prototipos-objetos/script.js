422f8c093143b22776e0fd11995234bfee2a2487
Maximo-sudo <maximovm2005@gmail.com>
2025-04-13 12:09:53 +0200

    Prototipo Parte 2

---
+++ 21-prototipos/1-prototipos-objetos/script.js
@@ -0,0 +1,8 @@
+function Libro(autor, titulo, cantPaginas) {
+    this.autor = autor;
+    this.titulo = titulo;
+    this.cantPaginas = cantPaginas;
+}
+
+let Libro1 = new Libro('Agatha Christie', 'Asesinato en el Orient Express', 256);
+
