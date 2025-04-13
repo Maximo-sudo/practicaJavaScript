cd57dc5d4a20b5dbebaa97aa29ad492df4ee12cb
Maximo-sudo <maximovm2005@gmail.com>
2025-04-13 11:55:25 +0200

    Prototipo Parte 1

---
+++ 21-prototipos/script.js
@@ -0,0 +1,7 @@
+// let persona = {dueño: 'Juan'};
+// let perro = Object.create(persona);
+// perro.nombre = 'Firulais';
+// let perro1 = Object.create(perro);
+
+let perro = {nombre: 'Firulais', raza: 'Yorkshire'};
+let perro1 = Object.create(perro);
