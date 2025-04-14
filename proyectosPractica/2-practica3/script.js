1b67b979ed23ec7de31f08cb980fbeb9a5e3dc0b
Maximo-sudo <maximovm2005@gmail.com>
2025-04-14 21:11:36 +0200

    Ya termine la parte de las pantallas segun la opción que selecciones

--- proyectosPractica/2-practica3/script.js
+++ proyectosPractica/2-practica3/script.js
@@ -11,7 +11,7 @@ let bloque1 = document.getElementById('bloque1');
 let bloque2 = document.getElementById('bloque2');
 let jsonData = {};
 
-
+// eventosMenu
 menuContainer.addEventListener('mouseenter', showMenu);
 menuContainer.addEventListener('mouseleave', hideMenu);
 function showMenu() {
@@ -21,6 +21,7 @@ function hideMenu() {
     menuContent.classList.remove('visible');
 };
 
+// manejoIntems
 menuItems.forEach(item => {
     item.addEventListener('click', (e) => {
         let value = e.target.dataset.value;
@@ -38,7 +39,7 @@ menuItems.forEach(item => {
 });
 
 function handleSelection(value) {
-    console.log('Selected value: ' + value);
+    console.log('Selected value: ', value);
 };
 
 function modeMenu(value) {
@@ -50,14 +51,41 @@ function modeMenu(value) {
         contMenu.classList.add('visible');
     } else if (value === 'peliculas.json') {
         bloque1.classList.add('visible');
-        // if (jsonData[value]) {
-        //     console.log('hola')
-        // }
+        chargeFormatMode(value, bloque1);
     } else if (value === 'series.json') {
         bloque2.classList.add('visible');
+        chargeFormatMode(value, bloque2);
     } 
     console.log('Mode menu activated with value:', value);
 };
+
+function chargeFormatMode(tipo, bloque) {
+    if (jsonData[tipo]) {
+        bloque.innerHTML = '';
+
+        const titulo = document.createElement('h1');
+        titulo.textContent = jsonData[tipo].titulo;
+
+        // heDeDarleFuncionalidadAlCrearLosOtrosJson
+        const content = document.createElement('p');
+        content.textContent = jsonData[tipo].content;
+
+        bloque.style.background = jsonData[tipo].fondo;
+        bloque.style.color = jsonData[tipo].colorTexto;
+        bloque.style.width = jsonData[tipo].ancho;
+        bloque.style.height = jsonData[tipo].alto;
+        bloque.style.boxShadow = jsonData[tipo].sombra;
+        bloque.style.padding = jsonData[tipo].padding;
+        bloque.style.margin = jsonData[tipo].margin;
+        bloque.style.borderRadius = jsonData[tipo].radiusBorder;
+
+        bloque.appendChild(titulo);
+        bloque.appendChild(content);
+    } else { 
+        console.warn('Datos No Encontrados Para: ', tipo);
+    }
+}
+
 
 function loadFormatMode() {
     fetch('json/formato.json')
